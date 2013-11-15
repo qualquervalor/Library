a9a09d0963501e9a2c7b0bf7232e0abae4241acb
Qualquer Valor <qualquervalor@gmail.com>
2013-11-15 12:31:08 -0600

    Updated documentation for Library, Borrower, and Book

--- library.rb
+++ library.rb
@@ -1,28 +1,41 @@
-# Public: A Library allows Various methods useful for performing mathematical operations.
-# All methods are module methods and should be called on the Math module.
+# Public: A Library maintains a catalog of books that are available for 
+#         Borrowers to borrow.
 #
-# Examples
-#
-#   Math.square_root(9)
-#   # => 3
 class Library
 
-  #Initialized the array of available books and 
-  #borrowed_book hash with books as keys and borrowers as values
+  # Public: Initialize Librarys catalog of available and borrowed books.
+  #
+  # books          - The array of available books.
+  # borrowed_books - Hash with Book as key and Borrower as value
   def initialize
     @books = []
     @borrowed_books = {}
   end
 
-  # This methods list all the books that our in the catalog.
-  # It first list the available books and then the checked out books
+  # Public: Prints out the catalog of books in the library with their status.
+  #
+  # Examples
+  #
+  #   list_books()
+  #   1. Finnegans Wake by James Joyce (Available)
+  #   2. The C Programming Language by Kernighan and Ritchie (Available)
   def list_books
     val = available_books
     borrowed_books(val)
   end
 
-  # This list the borrowed books passing in a count to number the output
-  def borrowed_books(count=1)
+  # Public: Prints out the checked out books in the library
+  #
+  # count - The interger at which to start counting the listed books 
+  #
+  # Examples
+  #
+  #   borrowed_books(4)
+  #   4. Nausea by Jean-Paul Sartre (Checked Out to Mike)
+  #   5. The Brothers Karamazov by Fyodor Dostoesvky (Checked Out to Gilbert)
+  #
+  # Returns the next count index for listing addtional books
+  def borrowed_books(count=1)  
     @borrowed_books.each do | k, v| 
       puts "#{count}. #{k.display_info} (Checked Out to #{v.name})"
       count+=1
@@ -30,7 +43,17 @@ class Library
     count
   end
 
-  # This list the available books passing in a count to number the output
+  # Public: Prints out the available books in the library
+  #
+  # count - The interger at which to start counting the listed books 
+  #
+  # Examples
+  #
+  #   available_books(4)
+  #   4. Nausea by Jean-Paul Sartre (Available)
+  #   5. The Brothers Karamazov by Fyodor Dostoesvky (Available)
+  #
+  # Returns the next count index for listing addtional books
   def available_books(count=1)
     @books.each do |x| 
       puts "#{count}. #{x.display_info} (Available)"
@@ -39,13 +62,18 @@ class Library
     count
  end
 
-  # This adds a book to the library's catalog
+  # Public: Adds a book to the lirary's catalog.
+  #
+  # book  - The book to be added.
   def add_book(book)
     @books.push book
   end
 
-  # This checks out a book from library's available book and updated 
+  # Public: This checks out a book from library's available book and updated 
   # the library system with who checked out the book.
+  #
+  # user - The Borrower who is trying to check out a book
+  # book - The book that is being requested
   def check_out(user, book)
 
     #check if valid inputs
@@ -73,7 +101,9 @@ class Library
     @borrowed_books[book] = user
   end
 
-  # Adds the book back to the Library available catalog
+  # Public: Adds the book back to the Library available catalog
+  #
+  # book - the book to be added back to the available catalog
   def check_in(book) 
      @books.push book
      who = @borrowed_books.delete(book)
@@ -83,37 +113,60 @@ class Library
   end
 end
 
-# Public: Various methods useful for performing mathematical operations.
-# All methods are module methods and should be called on the Math module.
+# Public: Defines a Borrower  who maintains a list of borrowed books
 #
 # Examples
 #
-#   Math.square_root(9)
-#   # => 3
+#   Borrower("bob")
 class Borrower
+
+  # Public: Initialize Borrower with name, books array and a count of number of books borrowed.
+  #
+  # name  - Name of the borrower
   def initialize(name)
     @name = name
     @books = []
     @book_count = 0
   end
 
+  #Public: Getter for borrowed books
+  #
+  #Returns array
   def borrowed_books
     @books
   end
 
+  #Public: Getter for name
+  #
+  #Returns name object
   def name
     @name
   end
 
+  #Public: Getter for book_count
+  #
+  #Returns integer representing the number of books borrowed by user
   def borrowed_books_count 
     @book_count
   end
+
+  #Public: increments book_count
   def increment_book_count
     @book_count+=1
   end
+
+  #Public: Decrementd book_count
   def decrement_book_count
     @book_count-=1
   end
+
+  # Public: Prints out the borrowed books of this user
+  #
+  # Examples
+  #
+  #   borrowed_books_list()
+  #   1. Nausea by Jean-Paul Sartre
+  #   2. The Brothers Karamazov by Fyodor Dostoesvky
   def borrowed_books_list
     count =1
     @books.each do |x| 
@@ -123,18 +176,26 @@ class Borrower
   end
 end
 
-# Public: Various methods useful for performing mathematical operations.
+# Public: Contains information about a Book, title and author
 # All methods are module methods and should be called on the Math module.
 #
 # Examples
 #
-#   Math.square_root(9)
-#   # => 3
+#   Book("My title","My Author")
 class Book
+
+  # Public: Initialize Book with title and author.
+  #
+  # title  - String with title of book
+  # author - String with author of the book
   def initialize(title, author)
     @title = title
     @author = author
   end
+
+  #Public: Display details about a book
+  #
+  # Return string containing book details
   def display_info
     "#{@title} by #{@author}"
   end
