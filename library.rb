8807f58a92796509c5d266b47229be8781817df3
Qualquer Valor <qualquervalor@gmail.com>
2013-11-15 10:11:49 -0600

    Updated Library's borrowed_book to be hash

--- library.rb
+++ library.rb
@@ -1,33 +1,51 @@
 class Library
   def initialize
     @books = []
+    @borrowed_books = {}
   end
 
-  def books
-  end
+  # def books
+  #   @books
+  # end
 
   def list_books
     count =1
     @books.each do |x| 
-      puts "#{count}. #{x.display_info}"
+      puts "#{count}. #{x.display_info} (Available)"
       count+=1
     end
+
+    @borrowed_books.each do | k, v| 
+      puts "#{count}. #{v.display_info} (Checked Out)"
+      count+=1
+    end
+
   end
 
-  def borrowed_books
-  end
+  # def borrowed_books
+  #   @borrowed_books
+  # end
 
-  def available_books
-  end
+  # def available_books
+  #   @books
+  # end
 
   def add_book(book)
     @books.push book
   end
 
   def check_out(user, book)
+    #check if valid inputs
+    #check if book is available
+    #check user book limit
+    #remove book from available
+    #add book to user borrowed book
+    #update library borrowed hash
   end
 
-  def check_in(book)
+  def check_in(book) 
+     @books.push book
+     @borrowed_books.delete(book)
   end
 end
 
